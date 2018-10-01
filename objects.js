36a53891c4fb0685de6a53382ef3ddcf04a36d14
Jason Basuil <basuilj@gmail.com>
2018-10-01 18:15:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,8 @@ function updatePlaylist(playlist, artist, title){
   playlist.artist = title
   return playlist
 }
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist
+  return playlist
+}
